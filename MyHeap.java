bca3e3e868adf7a4a14fa593745c397c7ba24937
Minuk Kim <mkim01@stuy.edu>
2019-05-03 01:15:58 -0400

    sorting test cases in main

--- MyHeap.java
+++ MyHeap.java
@@ -3,14 +3,13 @@ public class MyHeap{
   private int[] heap;
   private static void pushDown(int[] data, int size, int index){
    if (index * 2 + 2 < size){
-      if (data[index * 2 + 1] > data[index * 2 + 2]){
+      if (data[index * 2 + 1] > data[index]){
         //fompare with the left leaf
-        if (data[index] < data[index * 2 + 1]){
+        if (data[index * 2 + 2] < data[index * 2 + 1]){
           int temp = data[index];
           data[index] = data[index * 2 + 1];
           data[index * 2 + 1] = temp;
           pushDown(data, size, index * 2 + 2);
-        }
       }
       else{
         //compare with the right left
@@ -22,6 +21,7 @@ public class MyHeap{
         }
       }
     }
+  }
     //when there only exists one leaf
     else if(index * 2 + 1 < size){
       // there is only a left leaf
@@ -32,7 +32,6 @@ public class MyHeap{
         pushDown(data, size, index * 2 + 1);
       }
     }
-
     return;
   }
 
@@ -48,28 +47,62 @@ public class MyHeap{
   }
 
   public static void heapify(int[] data){
-    for(int i = data.length - 1 ; i >= 0; i--){
+    for(int i = data.length /2 ; i >= 0; i--){
       pushDown(data,data.length, i);
     }
   }
 
   public static void heapsort(int[] data){
+    int n = data.length;
     heapify(data);
-    System.out.println(Arrays.toString(data));
-    for (int i = data.length - 1; i > 0; i--){
-      int temp = data[i];
-      data[i] = data[0];
-      data[0] = temp;
-      pushDown(data, i, 0);
+    // System.out.println(Arrays.toString(data));
+    while (n > 0){
+    // for (int i = data.length - 1; i >= 0; i--){
+      int temp = data[0];
+      data[0] = data[n - 1];
+      data[n - 1] = temp;
+      n--;
+      pushDown(data, n, 0);
     }
   }
+  public static void main(String[]args){
+     System.out.println("Size\t\tMax Value\theapsort/builtin ratio ");
+     int[]MAX_LIST = {1000000000,500,10};
+     for(int MAX : MAX_LIST){
+       for(int size = 31250; size < 2000001; size*=2){
+         long qtime=0;
+         long btime=0;
+         //average of 5 sorts.
+         for(int trial = 0 ; trial <=5; trial++){
+           int []data1 = new int[size];
+           int []data2 = new int[size];
+           for(int i = 0; i < data1.length; i++){
+             data1[i] = (int)(Math.random()*MAX);
+             data2[i] = data1[i];
+           }
+           long t1,t2;
+           t1 = System.currentTimeMillis();
+           MyHeap.heapsort(data2);
+           t2 = System.currentTimeMillis();
+           qtime += t2 - t1;
+           t1 = System.currentTimeMillis();
+           Arrays.sort(data1);
+           t2 = System.currentTimeMillis();
+           btime+= t2 - t1;
+           if(!Arrays.equals(data1,data2)){
+             System.out.println("FAIL TO SORT!");
+             System.exit(0);
+           }
+         }
+         System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+       }
+       System.out.println();
+     }
+   // int[] ary = new int[] {34,52,12,43,78,90,2,31};
+   // heapify(ary);
+   // System.out.println(Arrays.toString(ary));
+   // HeapPrinter.print(ary);
+   // System.out.println(Arrays.toString(ary));
 
-  public static void main(String[] args) {
-   int[] ary = new int[] {34,52,12,43,78,90,2,31};
-   heapify(ary);
-   System.out.println(Arrays.toString(ary));
-   heapsort(ary);
-   System.out.println(Arrays.toString(ary));
- }
-
+}
 }
